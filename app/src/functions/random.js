5a2cc86355605340872705ef60bf8fe02b2dfc7f
mineejo <evgenyshaimov@gmail.com>
2022-05-21 05:03:19 +0300

    Added interesting random

---
+++ app/src/functions/random.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+	const _hFiguresMoves = document.getElementById('figures-moves-random');
+	if (_hFiguresMoves && CHESS_BOARD) {
+		
+		_generateMove();
+		
+		function _generateMove() {
+			const _sRandomLetter = CHESS_BOARD.LETTERS[_getRandomIntInclusive(0, Object.keys(CHESS_BOARD.LETTERS).length)];
+			const _sRandomNumber = CHESS_BOARD.NUMBERS[_getRandomIntInclusive(0, Object.keys(CHESS_BOARD.NUMBERS).length)];
+			const _sRandomEmotion = CHESS_BOARD.EMOTIONS[_getRandomIntInclusive(0, Object.keys(CHESS_BOARD.EMOTIONS).length)];
+			
+			if (_sRandomLetter && _sRandomNumber && _sRandomEmotion) {
+				_hFiguresMoves.innerHTML = `${_sRandomLetter}${_sRandomNumber}${_sRandomEmotion}`;
+			}
+			
+			setTimeout(_generateMove, _getRandomIntInclusive(1000, 3000));
+		}
+		
+		function _getRandomIntInclusive(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is
+			                                                          // inclusive
+		}
+	}
+});
